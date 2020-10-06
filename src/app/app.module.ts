55c1f2ce95a3d64fa73b62b533d86e13cd10dfea
santa clause <ochiengb215@gmail.com>
2020-10-06 11:56:36 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule} from '@angular/forms';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { FormsModule , ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -25,6 +25,9 @@ import { GithubRepoComponent } from './github-repo/github-repo.component';
     AppRoutingModule,
     FormsModule,
     HttpClientModule
+  ],
+  schemas: [
+   CUSTOM_ELEMENTS_SCHEMA
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/github-profile-data/github-profile-data.component.ts
+++ src/app/github-profile-data/github-profile-data.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-github-profile-data',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./github-profile-data.component.css']
 })
 export class GithubProfileDataComponent implements OnInit {
+  
+  @Input() githubProfile:any;
 
   constructor() { }
 
